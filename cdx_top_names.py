882c6f33778c83ebf4d6c03db21674c140877549
gandhis1 <siddhartha.a.gandhi@gmail.com>
2016-12-18 19:27:16 -0500

    various changes to test scripts plus CDX script

---
+++ cdx_top_names.py
@@ -0,0 +1,34 @@
+"""
+Author: Siddhartha Gandhi
+Date: 2016-12-18
+Description: Creates a table of the top traded CDX names.
+"""
+import bs4
+import re
+import requests
+import pandas as pd
+
+def parse(url, page_name):
+    """Given a DTCC URL and the name, parses the HTML table."""
+    table = (pd.read_html(url, attrs={'id' : 'dataTable'})[0]
+             .pipe(lambda df: df.loc[df.iloc[:, 5].str.isnumeric(), :])
+             .iloc[:, :8]
+             .assign(period=page_name))
+    table.columns = ['Reference Entity', 'Region', 'Index Constituent',
+                     'Total Number of Clearing Dealers',
+                     'Average Monthly Clearing Dealers',
+                     'Average Daily Notional (USD)',
+                     'Average Number of Trades per Day', 'DOC Clause %',
+                     "Period"]
+    return table
+
+source = r'http://www.dtcc.com/repository-otc-data/'
+soup = bs4.BeautifulSoup(requests.get(source).text, "html.parser")
+links = soup.find_all('a', href=re.compile('top-1000-single-names-'))
+data = (pd.concat([parse(source + '/'.join((link.get('href').split('/')[2:])),
+                         link.contents[0])
+                   for link in links[:2]])
+        .sort_values(by=['Average Daily Notional (USD)',
+                         'Average Number of Trades per Day'],
+                     ascending=False)
+        .reset_index(drop=True))
--- test_scripts.py
+++ test_scripts.py
@@ -1,15 +1,19 @@
-#%%
-import numpy as np
-import pandas as pd
-import sys
-import numpy
+""" 
+Includes various test scripts used for testing Python libraries, 
+statistical datasets, or IDE features.
+"""
 from random import randrange
 from timeit import timeit
+
+import numpy as np
+import pandas as pd
 from numba import jit
 
-#%%
+from typing import List
+
+
 @jit(nopython=True)
-def bubble_sort(raw_list):
+def bubble_sort(raw_list: List[int]):
     for i in range(0, len(raw_list)):
         is_sorted = True
         for j in range(0, len(raw_list) - i - 1):
@@ -22,7 +26,6 @@ def bubble_sort(raw_list):
             break
     return raw_list 
 
-#%%
 @jit(nopython=True)
 def quick_sort(raw_list):
     def quick_sort_recursive(raw_list, start, end):
@@ -34,38 +37,17 @@ def quick_sort(raw_list):
 
 # TODO - test concurrent futures and asyncIO
 
-#%%
-random_list = [randrange(0, 5000) for i in range(0, 5000)]
-time_bubble_sort = timeit(stmt="bubble_sort(random_list)",
-                          setup="from __main__ import bubble_sort, random_list",
-                          number=1000)
-time_python_sort = timeit(stmt="sorted(random_list)",
-                          setup="from __main__ import random_list",
-                          number=1000)
-print("Python sort: {0}".format(time_python_sort))
-print("Bubble sort: {0} ({1}x slower)".format(time_bubble_sort,
-                                              round(time_bubble_sort / time_python_sort, 1)))
-print("Sorting equivalence: {}".format(str(bubble_sort(random_list) == sorted(random_list))))
-
-#%% Weather manipulation
-import calendar
-import pandas as pd
-
-noaa_stations = {"NEW YORK CNTRL PK TWR" : "USW00094728",
-                 "CHICAGO UNIV" : "USW00014892"}
-
-# Get weather data, limit to Chicago University and New York Central Park Tower stations
-noaa_weather_data = r'ftp://ftp.ncdc.noaa.gov/pub/data/normals/1981-2010/products/temperature/dly-tavg-normal.txt'
-weather_data = pd.read_csv(noaa_weather_data, delim_whitespace=True, header=None)
-weather_data.columns = ["STATION", "MONTH"] + list(range(1, len(weather_data.columns) - 1))
-weather_data = (weather_data
-                    .loc[weather_data["STATION"].isin(noaa_stations.values())]
-                    .reset_index(drop=True)
-                    .set_index(["STATION", "MONTH"])
-                    .rename_axis("DAY", axis=1)
-                    .stack()
-                    .reset_index()
-                    .assign(MONTH = lambda x: list(map(lambda y: calendar.month_name[y], x["MONTH"]))))
-
-%matplotlib inline
-test = weather_data["DAY"].plot()
+rand_list = [randrange(0, 5000) for i in range(0, 5000)]
+t_bubble_sort = timeit(stmt="bubble_sort(rand_list)",
+                        setup="from __main__ import bubble_sort, rand_list",
+                        number=1000)
+t_python_sort = timeit(stmt="sorted(rand_list)",
+                        setup="from __main__ import rand_list",
+                        number=1000)
+print("Python sort: {0}".format(t_python_sort))
+print("{0} {1} ({2}x {3})".format("Bubble sort: ",
+                                    t_bubble_sort,
+                                    round(t_bubble_sort / t_python_sort, 1)),
+                                    "slower")
+sorting_equivalence = (bubble_sort(rand_list) == sorted(rand_list))
+print("Equivalent: {}".format(str(sorting_equivalence)))
